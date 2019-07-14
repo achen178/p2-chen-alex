7baf07241e85b1df5ae1df1e4ccd1fdf47855273
Alex Chen <32882075+achen178@users.noreply.github.com>
2019-07-14 19:48:47 -0400

    Add fallback for API limit reached

--- js/main.js
+++ js/main.js
@@ -12,17 +12,21 @@ dropdown.addEventListener("mouseout", function() {
 });
 
 var randomImage = document.getElementById('random');
+var attribution = document.getElementById("attribution")
+
+randomImage.src = "https://source.unsplash.com/1600x900/?puppy"
+randomImage.alt = "puppy"
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var apiResult = JSON.parse(this.responseText);
-
-        //Code Dealing With The API Data Goes Here
-        randomImage.src = apiResult.urls.regular;
-        randomImage.alt = apiResult.description;
-        console.log(apiResult.urls.regular);
-        console.log(apiResult.alt_description);
-    }
+  if (this.readyState == 4 && this.status == 200) {
+    var apiResult = JSON.parse(this.responseText);
+    randomImage.src = apiResult.urls.regular;
+    randomImage.alt = apiResult.description;
+    attribution.innerHTML = apiresult.user.name;
+    attribution.href = apiResult.user.links.self;
+    console.log(apiResult.urls.regular);
+    console.log(apiResult.alt_description);
+  }
 };
 xmlhttp.open('GET', 'https://api.unsplash.com/photos/random?query=puppy', true);
 xmlhttp.setRequestHeader("Accept-Version", "v1");
